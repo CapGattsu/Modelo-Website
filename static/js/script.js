bb4a8cafad9995fe10c7084a11d9b9c25dfaeb9f
CapGattsu <joovictorscarmato26@gmail.com>
2025-04-02 15:57:18 -0300

    update pagina de Blog

---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    speed: 500,
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    
+});
