f7afc67719d5e76aa981af022b418db494f5424c
GLKaiky <kaikyfranca8@gmail.com>
2025-04-04 10:51:41 -0300

    feat: adicionar dropdown em noticias

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
+document.querySelectorAll('.categoria-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const dropdown = header.nextElementSibling;
+        const isOpen = dropdown.style.maxHeight;
+
+        // Fecha todos os outros dropdowns
+        document.querySelectorAll('.dropdown').forEach(d => {
+            d.style.maxHeight = null;
+        });
+
+        // Abre se estiver fechado
+        if (!isOpen || isOpen === "0px") {
+            dropdown.style.maxHeight = dropdown.scrollHeight + "px";
+        }
+    });
+});
+
+
 var swiper = new Swiper(".mySwiper", {
     loop: true,
     autoplay: {
@@ -19,3 +37,4 @@ var swiper = new Swiper(".mySwiper", {
     },
     
 });
+
